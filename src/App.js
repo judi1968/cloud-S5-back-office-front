5ade9299ab6623437a0a5792e4017b32f6afec2c
Judi <judiherinirina04@gmail.com>
2024-01-10 06:44:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Ajoutez ici la logique de gestion de connexion
-    console.log(`Login submitted with username: ${username} and password: ${password}`);
+    
+    try {
+      const response = await fetch('https://cloud-s5-metier-production.up.railway.app/login/admin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        console.log('Connexion réussie!');
+        // Ajoutez ici la logique après une connexion réussie
+      } else {
+        console.error('Échec de la connexion. Vérifiez vos informations.');
+        // Ajoutez ici la logique après une connexion échouée
+      }
+    } catch (error) {
+      console.error('Erreur lors de la connexion:', error);
+    }
   };
 
   return (
     <div className="login-container">
-      <img src='%PUBLIC_URL%/../assets/images/logo/logo_trial rgba(a0).png' alt='logo du VaikaNet'></img>
+      <img src="%PUBLIC_URL%/../assets/images/logo/logo_trial rgba(a0).png" alt="logo du VaikaNet" />
       <form className="login-form" onSubmit={handleLogin}>
         <label>
           Nom:
           <input
+            name='name'
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
@@ -27,6 +46,7 @@ const LoginForm = () => {
         <label>
           Mot de passe:
           <input
+            name='password'
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -34,7 +54,7 @@ const LoginForm = () => {
           />
         </label>
         <center>
-        <button>Connecte</button>
+          <button type="submit">Connecter</button>
         </center>
       </form>
     </div>
@@ -45,9 +65,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <center>
-        <LoginForm />
-      </center>
+        <center>
+          <LoginForm />
+        </center>
       </header>
     </div>
   );
