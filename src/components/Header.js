23cfec30dc29f053a8a861ea401819b9b08c69f0
Judi <judiherinirina04@gmail.com>
2024-01-12 17:06:39 +0300

    update_header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Assurez-vous d'importer le CSS de Bootstrap dans votre projet
 import { NavLink } from 'react-router-dom';
-
+import "./../assets/css/Header.css"
+const style = {
+    color : 'red'
+};
 const Header = () => {
   return (
-    <header className="container mt-12">
+    <header className="container mt-12" class="header">
         <div className="mt-12">
-            <NavLink to="/login">login</NavLink>
-            <NavLink to="/home">login</NavLink>
-
-        </div>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Liste des annonces</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">les elements necessaire</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tableau statistique</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#"><NavLink to="/home">login</NavLink></a>
+            </li>
+        </ul>    
+    </div>     
     </header>
   );
 };
