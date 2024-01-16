6cfd1faeb0c4742c1d8ac2474e1669dbd0d32019
Judi <judiherinirina04@gmail.com>
2024-01-16 22:58:20 +0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,54 +11,63 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    // Exemple de logique de connexion
-    // Pour l'instant, nous supposons que la connexion réussit si le nom n'est pas vide
-    if (name.trim() !== '') {
-      setLoginStatus('success');
-      navigate('/home'); // Redirection vers "/home"
-    } else {
+    try {
+      const response = await fetch('https://cloud-s5-metier-production.up.railway.app/log_admin_traitement', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, password }),
+      });
+
+      if (response.ok) {
+        setLoginStatus('success');
+        navigate('/home'); // Redirection vers "/home"
+      } else {
+        setLoginStatus('failure');
+      }
+    } catch (error) {
+      console.error('Erreur lors de la demande au serveur:', error);
       setLoginStatus('failure');
     }
   };
 
-  
-  return(
-
+  return (
     <div className="App">
-    <header className="App-header">
-      <center>
-      <div className={`login-container ${loginStatus === 'success' ? 'success' : loginStatus === 'failure' ? 'failure' : ''}`}>
-      <img src="%PUBLIC_URL%/../assets/images/logo/logo_trial rgba(a0).png" alt="logo du VaikaNet" />
-      <form className="login-form" onSubmit={handleLogin}>
-        <label>
-          Nom d'admin: 
-          <input
-            name='name'
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            />
-        </label>
-        <label>
-          Mot de passe:
-          <input
-            name='password'
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            />
-        </label>
+      <header className="App-header">
         <center>
-          <button type="submit">{loginStatus === 'success' ? 'Connexion réussie' : loginStatus === 'failure' ? 'Connexion échouée' : 'Connecter'}</button>
+          <div className={`login-container ${loginStatus === 'success' ? 'success' : loginStatus === 'failure' ? 'failure' : ''}`}>
+            <img src="%PUBLIC_URL%/../assets/images/logo/logo_trial rgba(a0).png" alt="logo du VaikaNet" />
+            <form className="login-form" onSubmit={handleLogin}>
+              <label>
+                Nom d'admin:
+                <input
+                  name='name'
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </label>
+              <label>
+                Mot de passe:
+                <input
+                  name='password'
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </label>
+              <center>
+                <button type="submit">{loginStatus === 'success' ? 'Connexion réussie' : loginStatus === 'failure' ? 'Connexion échouée' : 'Connecter'}</button>
+              </center>
+            </form>
+          </div>
         </center>
-      </form>
+      </header>
     </div>
-      </center>
-    </header>
-  </div>
-
-);
+  );
 };
+
 export default Login;
