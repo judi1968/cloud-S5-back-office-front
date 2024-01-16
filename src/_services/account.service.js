3188beb808bb3b15a8e5ee96cf72811a47719c0f
Judi <judiherinirina04@gmail.com>
2024-01-17 01:58:45 +0300

    Update account.service.js

--- src/_services/account.service.js
+++ src/_services/account.service.js
@@ -10,3 +10,7 @@ let isLogged = () => {
     let token = localStorage.getItem('token') // mety mreturn null
     return !!token // raha null dia mreturn false
 }
+
+export const accountService =  {
+    saveToken,logout,isLogged
+}
